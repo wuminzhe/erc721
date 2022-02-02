2d47c39994b966a39aab386aa555d83d392d4e39
Aki Wu <wuminzhe@gmail.com>
2022-02-02 13:39:49 +0800

    Initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,83 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  Artblocks,
+  Approval,
+  ApprovalForAll,
+  Transfer,
+  Transfer1,
+  Transfer2,
+  Transfer3,
+  Transfer4,
+  Transfer5,
+  Transfer6,
+  Transfer7
+} from "../generated/Artblocks/Artblocks"
+import { ExampleEntity } from "../generated/schema"
+
+export function handleApproval(event: Approval): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new ExampleEntity(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.owner = event.params.owner
+  entity.approved = event.params.approved
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.balanceOf(...)
+  // - contract.getApproved(...)
+  // - contract.isApprovedForAll(...)
+  // - contract.name(...)
+  // - contract.ownerOf(...)
+  // - contract.supportsInterface(...)
+  // - contract.symbol(...)
+  // - contract.tokenURI(...)
+  // - contract.tokenMetadata(...)
+}
+
+export function handleApprovalForAll(event: ApprovalForAll): void {}
+
+export function handleTransfer(event: Transfer): void {}
+
+export function handleTransfer1(event: Transfer1): void {}
+
+export function handleTransfer2(event: Transfer2): void {}
+
+export function handleTransfer3(event: Transfer3): void {}
+
+export function handleTransfer4(event: Transfer4): void {}
+
+export function handleTransfer5(event: Transfer5): void {}
+
+export function handleTransfer6(event: Transfer6): void {}
+
+export function handleTransfer7(event: Transfer7): void {}
